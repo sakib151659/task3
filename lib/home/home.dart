0e65470ec5d784e29820596500fc11952814855b
sakib <sakib15-1659@diu.edu.bd>
2022-02-06 16:50:35 +0600

    under appbar cards added

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
@@ -17,80 +18,195 @@ class _HomePageState extends State<HomePage> {
         children: <Widget>[
           Stack(
             children: <Widget>[
-              Container(
-                height: 121,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                      topRight: Radius.circular(0),
-                      bottomRight: Radius.circular(20.0),
-                      topLeft: Radius.circular(.0),
-                      bottomLeft: Radius.circular(20.0)),
-                  color: HexColor("#60E99C"),
-                ),
-
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                      topRight: Radius.circular(0),
-                      bottomRight: Radius.circular(20.0),
-                      topLeft: Radius.circular(.0),
-                      bottomLeft: Radius.circular(20.0)),
-                  color: HexColor("#1B3149"),
-                ),
-                height: 115,
-
-
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        padding: EdgeInsets.zero,
-                        child: IconButton(
-                          onPressed: (){
-                          },
-                          icon: Image.asset("assets/images/appLogo.png", height: 100, width: 127, color: Colors.greenAccent),
-
+              Stack(
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        height: 121,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                              topRight: Radius.circular(0),
+                              bottomRight: Radius.circular(20.0),
+                              topLeft: Radius.circular(.0),
+                              bottomLeft: Radius.circular(20.0)),
+                          color: HexColor("#60E99C"),
                         ),
+
                       ),
-                    ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                              topRight: Radius.circular(0),
+                              bottomRight: Radius.circular(20.0),
+                              topLeft: Radius.circular(.0),
+                              bottomLeft: Radius.circular(20.0)),
+                          color: HexColor("#1B3149"),
+                        ),
+                        height: 115,
 
-                    Expanded(
-                      flex: 4,
-                        child: Container()
-                    ),
 
-                    Expanded(
-                      flex: 3,
-                      child: Row(
-                        //mainAxisAlignment: MainAxisAlignment.end,
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 3,
+                              child: Container(
+                                padding: EdgeInsets.zero,
+                                child: IconButton(
+                                  onPressed: (){
+                                  },
+                                  icon: Image.asset("assets/images/appLogo.png", height: 100, width: 127,),
 
-                        children: <Widget>[
-                          IconButton(
-                            onPressed: (){
-                            },
-                            icon: Image.asset("assets/imageIcon/notification.png", height: 30, width: 30, color: Colors.greenAccent),
+                                ),
+                              ),
+                            ),
 
-                          ),
+                            Expanded(
+                                flex: 4,
+                                child: Container()
+                            ),
 
-                          IconButton(
-                            onPressed: (){
-                            },
-                            icon: Image.asset("assets/imageIcon/person.png", height: 30, width: 30, color: Colors.greenAccent),
+                            Expanded(
+                              flex: 3,
+                              child: Row(
+                                //mainAxisAlignment: MainAxisAlignment.end,
 
-                          ),
+                                children: <Widget>[
+                                  IconButton(
+                                    onPressed: (){
+                                    },
+                                    icon: Image.asset("assets/imageIcon/notification.png", height: 30, width: 30, color: Colors.greenAccent),
 
-                        ],
+                                  ),
+
+                                  IconButton(
+                                    onPressed: (){
+                                    },
+                                    icon: Image.asset("assets/imageIcon/person.png", height: 30, width: 30, color: Colors.greenAccent),
+
+                                  ),
+
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+
+
                       ),
+                      ////////////////// apbar container ended///////////////
+
+                    ],
+                  ),
+
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    margin: const EdgeInsets.fromLTRB(10, 100, 10, 10),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10),
+                          bottomRight: Radius.circular(20.0),
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(20.0)),
+                      color: Colors.white,
                     ),
-                  ],
-                ),
+                    //height: 100,
+                    child: Column(
+                      children: <Widget> [
+                        const Text("What you'd like to delivered?"),
+                        FittedBox(
+                          child: Row(
+                            children: <Widget>[
+                              InkWell(
+                                child: Column(
+                                  children: [
+                                    Card(
+                                      color: HexColor("#60E99C"),
+                                      elevation: 5,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(8)
+                                      ),
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Image.asset('assets/imageIcon/bag.png',height: 128, width: 120,),
 
+                                        ],
+                                      ),
+                                    ),
+                                    Text('Shop & Drop',style: TextStyle( fontSize: 16),),
+                                  ],
+                                ),
+                                onTap: () async{
+                                  //cttcUrl();
+                                  //Navigator.push(context, new MaterialPageRoute(builder: (context) => cttcDmp() ));
+                                },
+                              ),
 
+                              InkWell(
+                                child: Column(
+                                  children: [
+                                    Card(
+                                      color: HexColor("#60E99C"),
+                                      elevation: 5,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(8)
+                                      ),
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Image.asset('assets/imageIcon/hand.png',height: 128, width: 120,),
+
+                                        ],
+                                      ),
+                                    ),
+                                    Text('Collect & Delivers',style: TextStyle( fontSize: 16),),
+                                  ],
+                                ),
+                                onTap: () async{
+                                  //cttcUrl();
+                                  //Navigator.push(context, new MaterialPageRoute(builder: (context) => cttcDmp() ));
+                                },
+                              ),
+
+                              InkWell(
+                                child: Column(
+                                  children: [
+                                    Card(
+                                      color: HexColor("#60E99C"),
+                                      elevation: 5,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(8)
+                                      ),
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Image.asset('assets/imageIcon/car.png',height: 128, width: 120,),
+
+                                        ],
+                                      ),
+                                    ),
+                                    Text('Movers',style: TextStyle( fontSize: 16),),
+                                  ],
+                                ),
+                                onTap: () async{
+                                  //cttcUrl();
+                                  //Navigator.push(context, new MaterialPageRoute(builder: (context) => cttcDmp() ));
+                                },
+                              ),
+
+                            ],
+                          ),
+                        )
+
+                      ],
+                    ),
+
+                  ),
+                ],
               )
             ],
-          )
+          ),
         ],
       ),
 
